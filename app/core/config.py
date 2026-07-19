0c73d14a5907f5c1464b4dc496d34c04d7837457
n0556703375-hue <n0556703375@gmail.com>
2026-07-19 06:37:52 +0300

    Default Magnific to Nano Banana Pro 2K

--- app/core/config.py
+++ app/core/config.py
@@ -11,11 +11,8 @@ class Settings:
     openai_text_model = os.getenv("OPENAI_TEXT_MODEL", "gpt-5-mini")
     magnific_api_key = os.getenv("MAGNIFIC_API_KEY", "")
     magnific_api_base = os.getenv("MAGNIFIC_API_BASE", "https://api.magnific.com").rstrip("/")
-    magnific_image_model = os.getenv("MAGNIFIC_IMAGE_MODEL", "realism")
-    magnific_resolution = os.getenv("MAGNIFIC_RESOLUTION", "2k")
-    magnific_adherence = int(os.getenv("MAGNIFIC_ADHERENCE", "70"))
-    magnific_hdr = int(os.getenv("MAGNIFIC_HDR", "50"))
-    magnific_creative_detailing = int(os.getenv("MAGNIFIC_CREATIVE_DETAILING", "33"))
+    magnific_image_model = os.getenv("MAGNIFIC_IMAGE_MODEL", "nano-banana-pro")
+    magnific_resolution = os.getenv("MAGNIFIC_RESOLUTION", "2K")
     generated_media_path = Path(os.getenv("GENERATED_MEDIA_PATH", BASE_DIR / "generated"))
     port = int(os.getenv("PORT", "8000"))
 
